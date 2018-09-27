672c2695246b1eb81ccd858b49c5c3e545023b57
unknown <batmanthelegendly@gmail.com>
2018-09-27 19:45:06 -0400

    header

---
+++ header.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include<string.h>
+
+
+int strlen( char * s0);
+char * strcpy( char *dest, char *source );
+char * strncat( char *dest, char *source, int n);
+int strcmp( char *s1, char *s2 );
+char * strchr( char *s, char c );
