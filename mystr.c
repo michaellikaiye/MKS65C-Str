1dadd4237168ca0a80dcf5c7df0468cd661f28a0
unknown <batmanthelegendly@gmail.com>
2018-09-28 22:08:51 -0400

    cpy

--- mystr.c
+++ mystr.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <string.h>
 
@@ -8,12 +9,11 @@ int mystrlen(char * s0){
 }
 
 char * mystrcpy(char *s1, char *s2){
-  for(int c = 0; s2[c]; c++){
-    s1[c] = s2[c];
-  }
+  while(*s1++ = *s2++);
   return s1;
 }
 
+/*
 char * mystrncat( char *dest, char *source, int n){
 
 }
@@ -24,9 +24,28 @@ int mystrcmp( char *s1, char *s2 ){
 
 char * mystrchr( char *s, char c ){
 
-
 }
+*/
 
 int main(){
+  char lemon[] = "What two words do not rhyme";
+  char demon[] = "These words";
+  char lemon2[] = "What two words do not rhyme";
+  char demon2[] = "These words";
+
+  int testlen = strlen(lemon);
+  int testlen2 = mystrlen(lemon);
+
+  printf("strlen %d \n",testlen);
+  printf("mystrlen %d \n",testlen2);
+
+
+  strcpy(demon,lemon);
+  mystrcpy(demon2,lemon2);
+
+  printf("strcpy %s \n",demon);
+  printf("mystrcpy %s \n",demon2);
+
+
   return 0;
 }
