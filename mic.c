98bdf14aabe50dd352da6e316a282a24a4be892b
michaellikaiye <mye1@stuy.edu>
2018-09-29 12:52:36 -0400

    michael sat morn

--- mic.c
+++ mic.c
@@ -3,30 +3,38 @@
 char * mystrncat( char *dest, char *source, int n){
   int destLength = strlen(dest);
   int i;
-  for (i = 0; i < n && source[i] != '\0'; i++) {
+  for (i = 0; i < n && source[i] != '\0'; i++)
     *(dest + destLength + i) = *(source + i);
-    printf("%s\n", dest);
-    /* dest[destLength + i] = source[i]; */
-  }
   *(dest + destLength + i) = '\0';
   
   return dest;
 }
 
 int mystrcmp( char *s1, char *s2 ){
-  while(s1 != '\0' && s2 != '\0' && *s1 == *s2) {
+  while(*s1 != '\0' && *s2 != '\0' && *s1 == *s2) {
     s1++;
     s2++;
   }
 
-  return s1 -s2;
+  return *s1 - *s2;
 }
+
+char * mystrchr( char *s, char c ){
+  while(*s != '\0' && *s != c)
+    s++;
+
+  return s;
+}
+
 int main(){
   char a[10] = "dog";
   char* b = "hot";
   char* c = mystrncat(a,b,5);
   char* d = strncat(a,b,2);
   printf("%s,%s \n", c, d);
-  printf("%d \n", mystrcmp(dog,hot),); 
+  printf("%d.%d \n", mystrcmp("aba", "abA"),strcmp("ab","ab")); 
+  printf("%d.%d \n", mystrcmp(a,b), strcmp(a,b));
+  printf("%s,%s \n", mystrchr("so sad man",'a'), strchr("so sad man", 'a'));
+  
   return 0;
 }
--- mystr.c
+++ mystr.c
@@ -4,28 +4,44 @@
 
 int mystrlen(char * s0){
   int len = 0;
-  while(*s0++) len++;
+  while(*s0++) 
+    len++;
+
   return len;
 }
 
 char * mystrcpy(char *s1, char *s2){
   while(*s1++ = *s2++);
+
   return s1;
 }
 
-/*
 char * mystrncat( char *dest, char *source, int n){
-
+  int destLength = strlen(dest);
+  int i;
+  for (i = 0; i < n && source[i] != '\0'; i++)
+    *(dest + destLength + i) = *(source + i);
+  *(dest + destLength + i) = '\0';
+  
+  return dest;
 }
 
 int mystrcmp( char *s1, char *s2 ){
+  while(*s1 != '\0' && *s2 != '\0' && *s1 == *s2) {
+    s1++;
+    s2++;
+  }
 
+  return *s1 - *s2;
 }
 
 char * mystrchr( char *s, char c ){
+  while(*s != '\0' && *s != c)
+    s++;
 
+  return s;
 }
-*/
+
 
 int main(){
   char lemon[] = "What two words do not rhyme";
@@ -45,6 +61,16 @@ int main(){
 
   printf("strcpy %s \n",demon);
   printf("mystrcpy %s \n",demon2);
+
+  //BAD TESTS CHANGE PLEASE
+  char a[10] = "dog";
+  char* b = "hot";
+  char* c = mystrncat(a,b,5);
+  char* d = strncat(a,b,2);
+  printf("%s,%s \n", c, d);
+  printf("%d.%d \n", mystrcmp("aba", "abA"),strcmp("ab","ab")); 
+  printf("%d.%d \n", mystrcmp(a,b), strcmp(a,b));
+  printf("%s,%s \n", mystrchr("so sad man",'a'), strchr("so sad man", 'a'));
 
 
   return 0;
