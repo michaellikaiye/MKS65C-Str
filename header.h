26a52240be6aed7a9b7a7333aef3193271bc5c5c
KevinLyy <33733915+KevinLyy@users.noreply.github.com>
2018-09-30 01:46:00 -0400

    Delete header.h

--- header.h
+++
@@ -1,7 +0,0 @@
-
-
-int mystrlen( char * s0);
-char * mystrcpy( char *dest, char *source );
-char * mystrncat( char *dest, char *source, int n);
-int mystrcmp( char *s1, char *s2 );
-char * mystrchr( char *s, char c );
